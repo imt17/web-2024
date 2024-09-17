38bfb90a85a259ed851cc4ee3a4a373761ee83aa
imt17 <157044776+imt17@users.noreply.github.com>
2024-09-17 21:34:28 +0300

    all deleted

--- christmasapp/views.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-from django.shortcuts import render
-
-def index(request):
-    today = datetime.now()
-    answer = today.month == 12 and today.day == 25
-    return render(request, "index.html", {'answer':answer})
--- secret_santa/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from secret_santa import views
-from christmasapp import views as christmas_views
-
-urlpatterns = [
-    path('secret_santa', views.secret_santa, name='secret_santa'),
-    path('', christmas_views.index, name='index'),
-]
